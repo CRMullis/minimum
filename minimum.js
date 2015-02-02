51b334afdd336b858bf4318d533900c3ca52965e
Collin Mullis <cmullis@saintjoe.edu>
2015-02-01 19:19:22 -0500

    Created program and added comments to program.

---
+++ minimum.js
@@ -0,0 +1,13 @@
+/* Collin Mullis
+	Ch.3 Minimum Function Homework
+	Last work on February 1st 2015
+*/
+//Creates function to give the smallest of 2 pre-defined numbers.
+function minimum(x,y) {
+	if (x < y)
+		return x;
+	else if (y < x)
+		return y;
+	}
+
+console.log(minimum(12,4003)); 
